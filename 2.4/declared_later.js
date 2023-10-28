0caf6d2508e665160e718dff89deced64ee6046b
VALANDPIYUSH18 <piyushvaland0000@gmail.com>
2023-10-28 20:21:19 +0530

    Arreges files

--- 2.4/declared_later.js
+++
@@ -1,8 +0,0 @@
-//if strict mode is not enabled than we can declare a var variable even after its assignment
-
-a = 10;
-var a; // declared later
-console.log(a); // it will print 10 the value of a
-
-b = 20;
-let b; // this will give error because we cant declare a let after its assinment
