783e01d1a1c96a26c3d5a6b3fe252dba3227a8ff
VALANDPIYUSH18 <piyushvaland0000@gmail.com>
2023-11-03 00:29:07 +0530

    objetsmethod

---
+++ part-1(4)/4.4/objectmethodthis.js
@@ -0,0 +1,15 @@
+// object 
+
+let person ={
+    firstname:"piyush",
+    lastname: "kumar",
+};
+ 
+person.age= 23;
+
+person.sayHello = function () {
+    console.log("hello !");
+    
+}
+ 
+person.sayHello
